223e90eb400a0efd94bfa2239ee7badc9c02614e
pranshu gupta <152961642+pranshu1411@users.noreply.github.com>
2025-06-26 23:17:56 +0530

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Pranshu_1404:Pranshu%401404@cluster0.dtyt3.mongodb.net/Course_selling_app?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const AdminSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
